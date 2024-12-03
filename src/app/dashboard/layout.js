11942d363e4fb5ac35f44f229f5a2c244cf28fd5
hakimasrori24 <hakimasrori24@gmail.com>
2024-12-03 07:22:33 +0000

    first commit

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,44 +1,43 @@
-"use client";
-
 import { Footer, Header, Navbar } from "@/_components/layout";
 import { CODE, SERVICE } from "@/_constant/api";
-import axios from "axios";
-import Cookies from "js-cookie";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+// import axios from "axios";
+// import Cookies from "js-cookie";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
 
 export default function DashboardLayout({ children }) {
-  const $router = useRouter();
+  // const $router = useRouter();
 
-  const checkAuthorize = async () => {
-    const token = Cookies.get("TTPG");
+  // const checkAuthorize = async () => {
+  //   const token = Cookies.get("TTPG");
 
-    try {
-      const response = await axios.post(
-        SERVICE.AUTH.PROFILE,
-        {
-          uuid: "2de5e6dfd04e49e58283485cbcdab3fa",
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+  //   try {
+  //     const response = await axios.post(
+  //       SERVICE.AUTH.PROFILE,
+  //       {
+  //         uuid: "2de5e6dfd04e49e58283485cbcdab3fa",
+  //       },
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       }
+  //     );
 
-      if (response.data.status != CODE.SUCCESS) {
-        Cookies.remove("TTPG");
-        $router.push("/sign");
-      }
-    } catch (error) {
-      Cookies.remove("TTPG");
-      $router.push("/sign");
-    }
-  };
+  //     if (response.data.status != CODE.SUCCESS) {
+  //       Cookies.remove("TTPG");
+  //       $router.push("/sign");
+  //     }
+  //   } catch (error) {
+  //     Cookies.remove("TTPG");
+  //     $router.push("/sign");
+  //   }
+  // };
 
-  useEffect(() => {
-    checkAuthorize();
-  }, []);
+  // useEffect(() => {
+  //   checkAuthorize();
+  // }, []);
+
   return (
     <div className="page">
       <Header />
